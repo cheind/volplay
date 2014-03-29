f8c5c6e320dd6f61ebb76db1f902286e88a38a79
Christoph Heindl <christoph.heindl@gmail.com>
2014-03-29 19:20:00 +0100

    Added SDFPlane
    Currently reflects only a xy-plane through origin.

---
+++ inc/volplay/sdf_plane.h
@@ -0,0 +1,34 @@
+// This file is part of volplay, a library for interacting with
+// volumetric data.
+//
+// Copyright (C) 2014 Christoph Heindl <christoph.heindl@gmail.com>
+//
+// This Source Code Form is subject to the terms of the BSD 3 license.
+// If a copy of the BSD was not distributed with this file, You can obtain
+// one at http://opensource.org/licenses/BSD-3-Clause.
+
+#ifndef VOLPLAY_SDF_PLANE
+#define VOLPLAY_SDF_PLANE
+
+#include <volplay/types.h>
+#include <volplay/sdf_node.h>
+
+namespace volplay {
+
+    /** A sphere primitive centered around the origin. */
+    class SDFPlane : public SDFNode {
+    public:
+        /** Create a new xy-plane. */
+        SDFPlane();
+        
+        /** Evaluate the SDF at given position. */
+        virtual SDFResult fullEval(const Vector &x) const;
+        
+    private:
+        Vector _normal;
+        Scalar _w;
+    };
+
+}
+
+#endif
---
+++ src/sdf_plane.cpp
@@ -0,0 +1,25 @@
+// This file is part of volplay, a library for interacting with
+// volumetric data.
+//
+// Copyright (C) 2014 Christoph Heindl <christoph.heindl@gmail.com>
+//
+// This Source Code Form is subject to the terms of the BSD 3 license.
+// If a copy of the BSD was not distributed with this file, You can obtain
+// one at http://opensource.org/licenses/BSD-3-Clause.
+
+#include <volplay/sdf_plane.h>
+
+namespace volplay {
+    
+    SDFPlane::SDFPlane()
+    : _normal(0,0,1), _w(0)
+    {}
+    
+    SDFResult
+    SDFPlane::fullEval(const Vector &x) const
+    {
+        SDFResult r = {this, x.dot(_normal) + _w};
+        return r;
+    }
+    
+}
